15822a17678f9c9f84187d836dd90b7d35c2e4a3
John Erwin <55669767+J-Erwin@users.noreply.github.com>
2020-11-24 20:38:09 -0600

    Update login.php

--- public_html/login.php
+++ public_html/login.php
@@ -18,7 +18,7 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
     $db = new DB;
     if($db->check_password($_POST['username'], $_POST['password'])) {
         session_start();
-        $_SESSION['id'] = $db->get_user_id($_POST['username']);
+        $_SESSION['user_id'] = $db->get_user_id($_POST['username']);
         $_SESSION['auth_status'] = true;
         header("Location: $home/index.php", true, 303);
         die ("<html><body>You have successfully logged in. <a href=\"index.php\">Click here if your are not redirected automatically.</a></body></html>\n");
