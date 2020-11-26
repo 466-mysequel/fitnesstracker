9b4d0ac010861d02dd6ad57e34d376bf6f69f089
Migi Quinones <z1868762@students.niu.edu>
2020-11-25 19:42:05 -0600

    convert function

--- src/library.php
+++ src/library.php
@@ -77,4 +77,96 @@ function is_authenticated(): bool {
     return isset($_SESSION['auth_status']) && $_SESSION['auth_status'];
 }
 
+
+
+
+/**
+ * Convert from one unit to another
+ * 
+ * This function first converts the beginning unit to Milliliters/Cubic Centimeters
+ * by multiplicating the conversion ratio from the beginning unit to Milliliters then divides
+ * by the conversion ratio from the milliliters to the end Unit.
+ * 
+ * @author z1868762
+ * @param qty - the amount of the beginning unit that needs to be converted
+ * @param beginUnit - the symbol unit before converting
+ * @param endUnit - the symbol unit after converting
+ * @return float - the quantity of the end unit after conversion. Returns -1 on error
+ * @example - convertVolume(56.3, tsp, L); - Converts 56.3 tablespoons into Liters
+ * @example - convertVolume(13, oz, pt); - Converts 13 fluid ounces into pints
+ * @see "Project Issue #41"
+ * 
+ */
+function convertVolume(float $qty, string $beginUnit, string $endUnit)
+{
+    $literRatio = 1000;
+    $gallonRatio = 3785.41;
+    $quartRatio = 946.353;
+    $pintRatio = 473.176;
+    $cupRatio = 236.588;
+    $ounceRatio = 29.5735;
+    $tbspRatio = 14.7868;
+    $tspRatio = 4.92892;
+    //since we're converting to milliliters, no math needs to be done if begin unit is mililiters
+    if($beginUnit == "mL")  {   $value = $qty;  }
+
+    //liters to milliliters
+    else if($beginUnit == "L")      {   $value = $qty * $literRatio;    }
+
+    //gallons to milliliters
+    else if($beginUnit == "gal")    {   $value = $qty * $gallonRatio;   }
+
+    //quart to milliliters
+    else if($beginUnit == "qt")     {   $value = $qty * $quartRatio;    }
+
+    //pint to milliliters
+    else if($beginUnit == "pt")     {   $value = $qty * $pintRatio;     }
+
+    //cup to milliliters
+    else if($beginUnit == "cup")    {   $value = $qty * $cupRatio;      }
+
+    //ounces to milliliters
+    else if($beginUnit == "oz")     {   $value = $qty * $ounceRatio;    }
+
+    //tablespoon to milliliters
+    else if($beginUnit == "tbsp")   {   $value = $qty * $tbspRatio;     }
+
+    //teaspoon to milliliters
+    else if($beginUnit == "tsp")    {   $value = $qty * $tspRatio;      }
+
+    //invalid beginning unit
+    else    {   echo "Beginning unit not found";    return -1;  }
+
+    //converting from mililliters to ending unit with division
+
+    //if user wants milliliters, no change needed
+    if($endUnit == "mL")   {   return $value;  }
+
+    //milliliters to liters
+    else if($endUnit == "L")        {   return $value/$literRatio;      }
+
+    //milliliters to gallons
+    else if($endUnit == "gal")      {   return $value/$gallonRatio;     }
+
+    //milliliters to quart 
+    else if($endUnit == "qt")       {   return $value/$quartRatio;      }
+
+    // milliliters to pint 
+    else if($endUnit == "pt")       {   return $value/$pintRatio;       }
+
+    // milliliters to cup  
+    else if($endUnit == "cup")      {   return $value/$cupRatio;        }
+
+    //milliliters to ounces         
+    else if($endUnit == "oz")       {   return $value/$ounceRatio;      }
+
+    //milliliters to tablespoon 
+    else if($endUnit == "tbsp")     {   return $value/$tbspRatio;       }
+
+    //milliliters to teaspoons  
+    else if($endUnit == "tsp")      {   return $value/$tspRatio;         }
+
+    //invalid ending unit
+    else    {   echo "ending unit not found";   return -1;  }
+}
 ?>
