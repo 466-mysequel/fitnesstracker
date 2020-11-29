cfb01438ea08116020ea127aa3a7ad846e3951b5
Harshith Reddy <harryreddy@comcast.net>
2020-11-29 14:09:31 -0600

    Create a new type of workout page

--- public_html/workouts.php
+++ public_html/workouts.php
@@ -57,7 +57,55 @@ if(isset($_GET['action'])):
 <?php   break;
         case 'new': ?>
         <h2>Create a new type of workout</h2>
+        <?php 
+        if(isset($_POST['mets_value']) && isset($_POST['category']) && isset($_POST['activity']) && isset($_POST['intensity'])){
+        if(!empty($_POST['mets_value']) && !empty($_POST['category']) && !empty($_POST['activity']) && !empty($_POST['intensity']))
+        {  
+            $update_workout = (bool) $db->add_workout_type($_POST['mets_value'], $_POST['category'], $_POST['activity'], $_POST['intensity'], $_POST['desctiption']);
+        }
+        //if there is an invalid entry will print that the nutrient was unsuccessfully added
+        else 
+        {
+         $update_workout= false;
+        }
+}?>
         <div class="row">
+        <div class="col-6">
+                <p class="lead">Enter Workout info</p>
+                <?php if(isset($update_workout)) echo $update_workout ? "<p style=\"color: #33aa33\">Workout sucessfully updated</p>\n" : "<p style=\"color: #aa3333\">Workout unsuccessfully updated</p>\n"; ?>
+                <form method="POST" class="form-inline">
+                <label for="Workout" class="sr-only">Workout</label>
+                    <input type="text" id="activity" name="activity" class="form-control" placeholder="Workout Name">
+                    <label for="Workout" class="sr-only">Mets Value</label>
+                    <input type="text" id="mets_value" name="mets_value" class="form-control" placeholder="Mets Value">
+
+                    <select class="form-control" name="category" id="category">
+                        <option value="" disabled selected hidden>Category</option>
+                        <option value="bicycling">Bicycling</option>
+                        <option value="conditioning excerise">Conditioning Excerise</option>
+                        <option value="dancing">Dancing</option>
+                        <option value="fishing and hunting">Fishing/Hunting </option>
+                        <option value="home activites">Home activites </option>
+                        <option value="home repair">Home repair </option>
+                        <option value="lawn and garden">Lawn and Garden</option>
+                        <option value="inactivity quiet/light">Light activities</option>
+                        <option value="miscellaneous">Miscellaneous</option>
+                        <option value="occupation">Occupation</option>
+                        <option value="music playing">Playing Music </option>
+                        <option value="religious activities">Religious Activities </option>
+                        <option value="running">Running</option>
+                        <option value="self care">Self care</option>
+                        <option value="sexual activity">Sexual activity</option>
+                        <option value="sports">Sports</option>
+                        <option value="transportation">Transportation</option>
+                        <option value="walking">Walking</option>
+                        <option value="water activities">Water Activites</option>
+                        <option value="winter activities">Winter Activities</option>  
+                        <option value="volunteer activities">Volunteer Work</option> 
+                    </select>
+                    <button class="btn btn-lg btn-primary" type="submit">Add Nutrient</button>
+                </form>
+            </div>
         </div><?php
         break;
     endswitch; ?>
