1bce6c2e025db84ba69b4fc28ae00b4744b9bd8a
Greg <z1871157@students.niu.edu>
2020-11-30 00:59:05 -0600

    Add jquery to add/remove intensities

--- public_html/workouts.php
+++ public_html/workouts.php
@@ -14,6 +14,35 @@ $page_title = "Workouts";
 include '../templates/header.php';
 $db = new DB();
 ?>
+<script>
+var intensityrow = `
+
+                        <div class="form-row">
+                            <div class="form-group col-md-1 text-right">
+                            	<a href="#" class="removeintensity" onclick="$(this).closest('div.form-row').remove();"><span style="font-size: 1.5em; color: transparent; text-shadow: 0 0 0 red; font-weight: bold;">&#x24E7;</span></a>
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label for="mets_value[]" class="sr-only">Mets Value</label>
+                                <input type="number" min="1.0" max="14.0" step="0.1" id="mets_value[]" name="mets_value[]" class="form-control" placeholder="Mets Value">
+                            </div>
+                            <div class="form-group col-md-8">
+                                <label for="intensity[]" class="sr-only">Intensity</label>
+                                <input type="text" id="intensity[]" name="intensity[]" class="form-control" placeholder="Intensity Description">
+                            </div>
+                        </div>
+`;
+$(document).ready(function(){
+  $("#addintensity").click(function(){
+    $("#intensities").append(intensityrow);
+  });
+  $(".removeintensity").click(function() {
+    $(this).parent('div').parent('div').remove();
+  });
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+});
+</script>
     <!-- Page Content -->
     <main role="main" class="container">
         <div class="row">
@@ -34,20 +63,20 @@ $db = new DB();
 if(isset($_GET['action'])):
     switch($_GET['action']):
         case 'log': ?>
-        <h2>Log a workout activity</h2>
+        <h1>Log a workout activity</h1>
         <div class="row">
         </div>
 <?php   break;
         case 'history': ?>
-        <h2>Your workout history</h2>
+        <h1>Your workout history</h1>
         <div class="row">
         </div>
 <?php   break;
         case 'browse': ?>
         <?php if(isset($_GET['id'])): ?>
-            <h2>Workout <?php echo $_GET['id'] ?></h2>
+            <h1>Workout <?php echo $_GET['id'] ?></h1>
         <?php else: ?>
-            <h2>Directory of workouts</h2>
+            <h1>Directory of workouts</h1>
             <ul><?php foreach($db->get_workout_types() as $id => $name): ?>
                 <li><a href="?action=browse&id=<?php echo $id ?>"><?php echo htmlspecialchars($name) ?></li>
             <?php endforeach; ?></ul>
@@ -56,7 +85,7 @@ if(isset($_GET['action'])):
         </div>
 <?php   break;
         case 'new': ?>
-        <h2>Create a new type of workout</h2>
+        <h1>Create a new type of workout</h1>
         <?php
         $new_workouts = []; 
         if(isset($_POST['mets_value']) && isset($_POST['category']) && isset($_POST['activity']) && isset($_POST['intensity'])){
@@ -81,7 +110,7 @@ if(isset($_GET['action'])):
         }
 }?>
         <div class="row">
-        <div class="col-6">
+        <div class="col-8">
                 <p class="lead">Enter Workout info</p>
                 <?php 
                     if(isset($update_workout)) { 
@@ -98,64 +127,97 @@ if(isset($_GET['action'])):
                     }
                 ?>
                 <form method="POST">
-                    <select class="form-control" name="category" id="category">
-                        <option value="" disabled selected hidden>Category</option>
-                        <option value="bicycling">Bicycling</option>
-                        <option value="conditioning excerise">Conditioning Excerise</option>
-                        <option value="dancing">Dancing</option>
-                        <option value="fishing and hunting">Fishing/Hunting </option>
-                        <option value="home activites">Home activites </option>
-                        <option value="home repair">Home repair </option>
-                        <option value="lawn and garden">Lawn and Garden</option>
-                        <option value="inactivity quiet/light">Light activities</option>
-                        <option value="miscellaneous">Miscellaneous</option>
-                        <option value="occupation">Occupation</option>
-                        <option value="music playing">Playing Music </option>
-                        <option value="religious activities">Religious Activities </option>
-                        <option value="running">Running</option>
-                        <option value="self care">Self care</option>
-                        <option value="sexual activity">Sexual activity</option>
-                        <option value="sports">Sports</option>
-                        <option value="transportation">Transportation</option>
-                        <option value="walking">Walking</option>
-                        <option value="water activities">Water Activites</option>
-                        <option value="winter activities">Winter Activities</option>  
-                        <option value="volunteer activities">Volunteer Work</option> 
-                    </select>
-                    <label for="activity" class="sr-only">Activity Name</label>
-                    <input type="text" id="activity" name="activity" class="form-control" placeholder="Activity Name">
                     <div class="form-row">
                         <div class="form-group col-md-12">
-                            Intensities
+                            <h4>Workout type</h4>
                         </div>
                     </div>
                     <div class="form-row">
-                        <div class="form-group col-md-3">
-                            <label for="mets_value[]" class="sr-only">Mets Value</label>
-                            <input type="number" min="1.0" max="14.0" step="0.1" id="mets_value[]" name="mets_value[]" class="form-control" placeholder="Mets Value">
+                        <div class="form-group col-md-4">
+                            <label for="category"><h5>Category</h5></label>
                         </div>
-                        <div class="form-group col-md-9">
-                            <label for="intensity[]" class="sr-only">Intensity</label>
-                            <input type="text" id="intensity[]" name="intensity[]" class="form-control" placeholder="Intensity Description">
+                        <div class="form-group col-md-8">
+                            <label for="activity"><h5>Activity name</h5></label>
                         </div>
                     </div>
                     <div class="form-row">
-                        <div class="form-group col-md-3">
-                            <label for="mets_value[]" class="sr-only">Mets Value</label>
-                            <input type="number" min="1.0" max="14.0" step="0.1" id="mets_value[]" name="mets_value[]" class="form-control" placeholder="Mets Value">
+                        <div class="form-group col-md-4 text-right">
+                            <select class="form-control" name="category" id="category">
+                                <option value="" disabled selected hidden>Category</option>
+                                <option value="bicycling">Bicycling</option>
+                                <option value="conditioning excerise">Conditioning Excerise</option>
+                                <option value="dancing">Dancing</option>
+                                <option value="fishing and hunting">Fishing/Hunting </option>
+                                <option value="home activites">Home activites </option>
+                                <option value="home repair">Home repair </option>
+                                <option value="lawn and garden">Lawn and Garden</option>
+                                <option value="inactivity quiet/light">Light activities</option>
+                                <option value="miscellaneous">Miscellaneous</option>
+                                <option value="occupation">Occupation</option>
+                                <option value="music playing">Playing Music </option>
+                                <option value="religious activities">Religious Activities </option>
+                                <option value="running">Running</option>
+                                <option value="self care">Self care</option>
+                                <option value="sexual activity">Sexual activity</option>
+                                <option value="sports">Sports</option>
+                                <option value="transportation">Transportation</option>
+                                <option value="walking">Walking</option>
+                                <option value="water activities">Water Activites</option>
+                                <option value="winter activities">Winter Activities</option>  
+                                <option value="volunteer activities">Volunteer Work</option> 
+                            </select>
                         </div>
-                        <div class="form-group col-md-9">
-                            <label for="intensity[]" class="sr-only">Intensity</label>
-                            <input type="text" id="intensity[]" name="intensity[]" class="form-control" placeholder="Intensity Description">
+                        <div class="form-group col-md-8 text-right">
+                            <label for="activity" class="sr-only">Activity Name</label>
+                            <input type="text" id="activity" name="activity" class="form-control" placeholder="Activity Name">
                         </div>
                     </div>
-                    <button class="btn btn-lg btn-primary" type="submit">Add Workout</button>
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <h4>Intensities</h4>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-1">
+                        </div>
+                        <div class="form-group col-md-3 w3tooltip">
+                            <h5>MET Value <span data-toggle="tooltip" title="A MET is a ratio of your working metabolic rate relative to your resting metabolic rate." style="color: transparent; text-shadow: 0 0 0 #4f6cbd; font-weight: bold;">&#9432;</span></h5>
+                        </div>
+                        <div class="form-group col-md-8">
+                        <h5>Intensity Description</h5>
+                        </div>
+                    </div>
+                    <div id="intensities">
+                        <div class="form-row">
+                            <div class="form-group col-md-1">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label for="mets_value[]" class="sr-only">Mets Value</label>
+                                <input type="number" min="1.0" max="14.0" step="0.1" id="mets_value[]" name="mets_value[]" class="form-control" placeholder="1">
+                            </div>
+                            <div class="form-group col-md-8">
+                                <label for="intensity[]" class="sr-only">Intensity</label>
+                                <input type="text" id="intensity[]" name="intensity[]" class="form-control" placeholder="eg. low, medium, high, casual, or competitive">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-1 mets text-right">
+                        	<label for="addintensity"><span style="font-size: 1em; color: transparent; text-shadow: 0 0 0 green; text-align: right">&#x2795;</span></label>
+                        </div>
+                        <div class="form-group col-md-3 mets">
+                            <button type="button" id="addintensity" class="btn btn-secondary">Add intensity</button>
+                        </div>
+                        <div class="form-group col-md-8">
+                            
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <button class="btn btn-lg btn-primary" type="submit">Add Workout</button>
+                        </div>
+                    </div>
                 </form>
-                <pre>
-                <?php
-                    var_dump($_POST);
-                ?>
-                </pre>
             </div>
         </div><?php
         break;
@@ -167,7 +229,7 @@ if(isset($_GET['action'])):
 
             </div>
             <div class="col-6">
-                <h2>Lorem ipsum dolar sil imet</h2>
+                <h1>Lorem ipsum dolar sil imet</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </div>
