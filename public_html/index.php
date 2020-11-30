9585fd274488e5dbb9cd0d0dab8f724a5b99c22e
Harshith Reddy <harryreddy@comcast.net>
2020-11-30 17:14:37 -0600

    Signed out version of homepage

--- public_html/index.php
+++ public_html/index.php
@@ -13,26 +13,40 @@ include_once '../src/library.php';
 $page_title = "Fitness Tracker &rsaquo; Home Page";
 include '../templates/header.php';
 $db = new DB();
+
+echo<<<HTML
+<style> 
+.signed-out {
+  background-image: url('https://images.unsplash.com/photo-1571902943202-507ec2618e8f');
+  background-color: #cccccc;
+  height: 500px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  background-attachment: fixed;
+}
+</style>
+HTML;
+
 ?>
     <!-- Page Content -->
-    <div class="jumbotron text-center">
+
+    <?php if(is_authenticated()): ?>
+        <div class="jumbotron text-center">
             <h1><?php echo $page_title; ?></h1>
-            <p class="lead">Coming soon!</p>
-    </div>
-    <main role="main" class="container">
-        <div class="row">
-            <div class="col-lg-10 text-center">
-                <h2>ER Diagram</h2>
-                <img src="../docs/erd.png" width="960" height="400" />
-            </div>
-            <div class="col-lg-2 text-center">
-                <h2>Database</h2>
-<?php $tables = $db->query("SHOW TABLES;")->fetchAll(PDO::FETCH_ASSOC); draw_table($tables, ["Tables"]); ?>
-            </div>
         </div>
-        <div class="row">
-            <h2>Lorem ipsum dolar sil imet</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <main role="main" class="container">
+            <h1> Signed in</h1>
+        </main>
+    <?php else: ?>
+        <div class="signed-out">
+            <div class="jumbotron text-center">
+                <h1><?php echo $page_title; ?></h1>
+            </div>
+            <main role="main" class="container">
+            <h1 style="color:#007bff">You need to be <a style="color:#eb3434" href="login.php">signed in</a> to view your stats!</h1>
+            </main>
         </div>
-    </main>
+    <?php endif; ?>
 <?php include '../templates/footer.php'; ?>
